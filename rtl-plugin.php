614db7733faa9aee7422da4741453724e8140472
espiat <m.spanier@espiat.com>
2017-05-31 16:19:17 +0200

    Update rtl-plugin.php

--- rtl-plugin.php
+++ rtl-plugin.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: RTL Meta Box
 * Plugin URI: 
-* Description: Add meta box to every page and post. if active on a page the css changes the body screen redaing to rtl (right ) for css 
+* Description: Add meta box to every page and post. If active on a page/post the css changes the body screen reading to rtl (right to left) for the_content & the_title. 
 * Version: 1.0
 * Author: espiat
 * Author URI: www.m.espiat.com
